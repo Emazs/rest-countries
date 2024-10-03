d4c005c7a9eb2386936c099ecc997d3c3deab823
Elias M. <121462341+Emazs@users.noreply.github.com>
2024-10-03 15:59:01 -0500

    Update UseFetchdata.js

--- src/components/UseFetchdata.js
+++ src/components/UseFetchdata.js
@@ -17,36 +17,3 @@ export const UseFetchdata = (URL_COUNTRYS, nameCountry) => {
 
     return { data, error ,loading }
 }
-
-
-/*
-export const UseFetchdata = async(URL_COUNTRYS, nameCountry) => {
-
-    const [data, setData] = useState([])
-
-    useEffect(() => {
-        const funFetch = async() => {
-            const res = await fetch(URL_COUNTRYS)
-            const data = await res.json()
-            setData(data)
-        }
-
-        funFetch()
-    }, [nameCountry])
-
-    return { data, setData }
-}
-
-export const UseFetchdata = (URL_COUNTRYS, nameCountry) => {
-
-    const [data, setData] = useState([])
-
-    useEffect(() => {
-        fetch(URL_COUNTRYS)
-            .then(res => res.json())
-            .then(datas => setData(datas))
-    }, [nameCountry])
-
-    return { data, setData }
-}
-*/
